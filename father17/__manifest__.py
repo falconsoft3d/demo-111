652dcaaa48890803804b12900c02374b0a40ecff
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2025-12-29 20:57:31 +0100

    sc

---
+++ father17/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Father MFH',
+    'version' : '1.2',
+    'summary': 'Invoices & Payments',
+    'sequence': 10,
+    'description': """
+Father (TOTP)
+================================
+Allows users to configure
+    """,
+    'category': 'Accounting/Accounting',
+    'website': 'https://www.marlonfalcon.com',
+    'depends': ['base'],
+    'category': 'Extra Tools',
+    'auto_install': False,
+    'data': [
+        'views/view.xml',
+    ],
+    'license': 'LGPL-3',
+}
